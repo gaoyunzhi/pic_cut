1df5316c22272ed2742e6fcaadf2de42a82e2fed
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-15 05:58:09 -0700

    commit

--- pic_cut/__init__.py
+++ pic_cut/__init__.py
@@ -13,34 +13,29 @@ def goodSize(w, h):
 	return h < 2 * w
 
 def computeSize(h, p):
-    raw_h = h / (p - MARGIN * (p - 1))
-    move = math.ceil(raw_h * (1 - MARGIN))
-    nh = h - (p - 1) * move
-    assert(raw_h - p + 1 <= nh)
-    assert(nh <= raw_h)
-    return nh, move
+	raw_h = h / (p - MARGIN * (p - 1))
+	nh = math.ceil(raw_h)
+	moves = [math.ceil(raw_h * ((1 - MARGIN) * step)) for step in range(p - 1)]
+	moves.append(h - nh)
+	return nh, moves
 
 def cut(path):
-    img = Image.open(path)
+	img = Image.open(path)
 
-    w, h = img.size
+	w, h = img.size
 
-    for piece in range(1, 10):
-    	nh, move = computeSize(h, piece)
-    	if goodSize(w, nh):
-    		break
-    if piece == 1 or not goodSize(w, nh):
-    	piece = 0
+	for piece in range(1, 10):
+		nh, moves = computeSize(h, piece)
+		if goodSize(w, nh):
+			break
 
-    upper = 0
-    lower = nh
+	if len(moves) <= 1 or not goodSize(w, nh):
+		moves = []
 
-    for p in range(piece):
-        working_slice = img.crop((0, upper, w, lower))
-        main_path, _ = os.path.splitext(path)
-        fn = '%s_%d.png' % (main_path, p)
-        working_slice.save(fn)
-        yield fn
-        upper += move
-        lower += move
+	for p, m in enumerate(moves):
+		working_slice = img.crop((0, m, w, m + nh))
+		main_path, _ = os.path.splitext(path)
+		fn = '%s_%d.png' % (main_path, p)
+		working_slice.save(fn)
+		yield fn
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pic_cut",
-    version="0.0.2",
+    version="0.0.3",
     author="Yunzhi Gao",
     author_email="gaoyunzhi@gmail.com",
     description="Cut Long Picture.",
