3ca8c5f15aa3d2bc4e5e25d0e9a58613b9d6b81b
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-15 05:39:06 -0700

    commit

--- pic_cut/__init__.py
+++ pic_cut/__init__.py
@@ -7,30 +7,33 @@ from PIL import Image
 import math
 import os
 
+MARGIN = 0.05
+
 def goodSize(w, h):
 	return h < 2 * w
 
 def computeSize(h, p):
-	return math.ceil(h / (p - 0.05 * (p - 1)))
+    raw_h = h / (p - MARGIN * (p - 1))
+    move = math.ceil(raw_h * (1 - MARGIN))
+    nh = h - (p - 1) * move
+    assert(raw_h - p + 1 <= nh)
+    assert(nh <= raw_h)
+    return nh, move
 
 def cut(path):
-    print('c1')
     img = Image.open(path)
-    print('c2')
 
     w, h = img.size
 
     for piece in range(1, 10):
-    	nh = computeSize(h, piece)
+    	nh, move = computeSize(h, piece)
     	if goodSize(w, nh):
     		break
     if piece == 1 or not goodSize(w, nh):
     	piece = 0
 
-    print(piece)
-
     upper = 0
-    lower = h - (piece - 1) * nh
+    lower = nh
 
     for p in range(piece):
         working_slice = img.crop((0, upper, w, lower))
@@ -38,6 +41,6 @@ def cut(path):
         fn = '%s_%d.png' % (main_path, p)
         working_slice.save(fn)
         yield fn
-        upper += nh
-        lower += nh
+        upper += move
+        lower += move
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pic_cut",
-    version="0.0.1",
+    version="0.0.2",
     author="Yunzhi Gao",
     author_email="gaoyunzhi@gmail.com",
     description="Cut Long Picture.",
